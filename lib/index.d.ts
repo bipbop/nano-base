f7959a8bc92bafc95176555fbe2fdb5d862bd8ae
Lucas Fernando Amorim <lf.amorim@yahoo.com.br>
2019-09-21 13:42:58 -0300

    types and lock

---
+++ lib/index.d.ts
@@ -0,0 +1,10 @@
+export namespace Nano {
+
+  interface Config {[key: string]: any}
+
+  interface Interface extends Config {
+    define<T>(name: string, value: T): void;
+  }
+}
+
+export default function (config?: Nano.Config): Nano.Interface
