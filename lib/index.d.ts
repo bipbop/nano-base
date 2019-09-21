cc5ebc308d1df02d782ca7586c7fa8171503c459
Lucas Fernando Amorim <lf.amorim@yahoo.com.br>
2019-09-21 13:49:57 -0300

    global type

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,10 +1,11 @@
-export namespace Nano {
+declare global {
+  export namespace Nano {
 
-  interface Config {[key: string]: any}
+    interface Config {[key: string]: any}
 
-  interface Interface extends Config {
-    define<T>(name: string, value: T): void;
+    interface Interface extends Config {
+      define<T>(name: string, value: T): void;
+    }
   }
 }
-
 export default function (config?: Nano.Config): Nano.Interface
